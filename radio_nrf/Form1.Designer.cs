38faf004bbfc50ae7d2eb5076617529c2fb78e35
Odin0chka <alex5osinin@gmail.com>
2018-02-19 15:59:44 +0700

    First commit

---
+++ radio_nrf/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace radio_nrf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 241);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 244);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(42, 340);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(520, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(42, 402);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 116);
+            this.listBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 45);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(101, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(417, 213);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 276);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serisl Port Communication";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(447, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(115, 276);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Option";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 630);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ radio_nrf/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+namespace radio_nrf
+{
+    public partial class Form1 : Form
+    {
+
+        private static Encoding enc8 = Encoding.UTF8;
+        private delegate void SetTextDeleg(string text);
+        string data;
+        public Form1()
+        {
+            InitializeComponent();
+            // получаем список доступных портов
+            string[] ports = SerialPort.GetPortNames();
+            // выводим список портов
+            foreach (string p in ports)
+                comboBox1.Items.Add(p);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!serialPort1.IsOpen)
+                serialPort1.Open();
+            //Открываем порт.
+            serialPort1.Write("1");
+            //Thread.Sleep(500);
+            /*while (serialPort1.BytesToRead <= 0)
+                if (serialPort1.BytesToRead > 0)
+                {
+                    string s = serialPort1.ReadExisting();
+                    /*byte[] data = new byte[serialPort1.BytesToRead];
+                    serialPort1.Read(data, 0, serialPort1.BytesToRead);
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        string s = enc8.GetString(data);
+                        richTextBox1.Text = s;
+                    }
+                    richTextBox1.Text += s;
+                }*/
+            //serialPort1.Close();
+        }
+
+        //void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    Thread.Sleep(500);
+        //    string data = serialPort1.ReadLine();
+        //    // Привлечение делегата на потоке UI, и отправка данных, которые
+        //    // были приняты привлеченным методом.
+        //    // ---- Метод "si_DataReceived" будет выполнен в потоке UI,
+        //    // который позволит заполнить текстовое поле TextBox.
+        //    this.BeginInvoke(new SetTextDeleg(si_DataReceived),
+        //                     new object[] { data });
+        //}
+
+        private void si_DataReceived(string data)
+        {
+            richTextBox1.Text += data;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.SelectedItem.ToString(); //Указываем наш порт
+            serialPort1.BaudRate = 9600; //указываем скорость.
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.None;
+            serialPort1.Handshake = Handshake.None;
+
+            try
+            {
+                //Открываем порт.
+                //byte[] data = { (byte)'c', (byte)'o', (byte)'n', (byte)'f', (byte)'i', (byte)'g' };
+                //port.Write(data, 0, data.Length);
+                /*Byte[] bytes = new Byte[10];
+                port.Read(bytes, 0, 5);
+                string s = enc8.GetString(bytes);
+                textBox1.Text = s;*/
+            }
+            catch (Exception)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Ошибка получения настроек!");
+                return;
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            /*SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            richTextBox1.Text += indata;*/
+            Thread.Sleep(50);
+            data = serialPort1.ReadExisting();
+            Thread.Sleep(50);
+            // Привлечение делегата на потоке UI, и отправка данных, которые
+            // были приняты привлеченным методом.
+            // ---- Метод "si_DataReceived" будет выполнен в потоке UI,
+            // который позволит заполнить текстовое поле TextBox.
+            BeginInvoke(new SetTextDeleg(si_DataReceived), new object[] { data });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialPort1.Close();
+        }
+    }
+}
